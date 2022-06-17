cd2c9e4decc24cae31f90adcd3587520b6269cce
selim-jo <skyblue.777@daum.net>
2022-06-17 19:52:36 +0900

    초기 세팅

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,26 @@
-const express = require('express')
+// 모듈
+const express = require('express');
 const router = express.Router();
+const Post = require('../schemas/post');
+const authMiddleware = require('../middlewares/auth-middleware');
 
+// 채용정보 등록(기업회원 로그인 시 가능)
+router.post('/posting', async (req, res) => {
+
+});
+
+// 채용정보 수정(기업회원 로그인 시 가능)
+router.put('/posting/:postingid', async (req, res) => {
+
+});
+
+// 채용정보 삭제(기업회원 로그인 시 가능)
+router.delete('/posting/:postingid', async (req, res) => {
+
+});
+// 채용정보 전체조회
+router.get('/posting', async (req, res) => {
+
+});
 
 module.exports = router;
