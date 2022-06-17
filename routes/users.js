5a7c7e0c8cfcd9749339885784681ee84b8e0e0b
hayeonkimm <hayeon.data@gmail.com>
2022-06-17 20:29:07 +0900

    companyuser 스키마 추가

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../schemas/user');
+const CompanyUser = require('../schemas/companyuser');
 const router = express.Router();
 const Joi = require('joi');
 const jwt = require('jsonwebtoken');
@@ -9,22 +10,22 @@ const authMiddleware = require('../middlewares/auth-middleware');
 const Bcrypt = require('bcrypt');
 dotenv.config();
 
-// const salt = await Bcrypt.genSalt(Number(process.env.SaltKEY));
 
 //회원가입 양식
 const postUsersSchema = Joi.object({
-  userId: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{2,10}$')).required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{4,20}$')).required(),
+  userid: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{2,10}$')).required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{4,20}$'))
+  .required(),
   confirmPassword: Joi.string().required(),
-  userImageUrl: Joi.string(),
+  profileimage: Joi.string(),
 });
 
-//회원가입
-router.post('/signup', async (req, res) => {
+//회원가입 - 개인
+router.post('/api/user/signup', async (req, res) => {
   try {
-    const { userId, password, confirmPassword, userImageUrl } =
+    const { userid, password, confirmPassword username, profileimage, position } =
       await postUsersSchema.validateAsync(req.body);
-    // console.log({ userId, password, confirmPassword, userImageUrl });
+    // console.log({ userid, password, confirmPassword, profileimage });
 
     if (password !== confirmPassword) {
       return res.status(400).send({
@@ -32,7 +33,7 @@ router.post('/signup', async (req, res) => {
       });
     }
 
-    const exitstUsers = await User.find({ userId });
+    const exitstUsers = await User.find({ userid });
     if (exitstUsers.length) {
       return res.status(400).send({
         errorMessage: '중복된 아이디가 존재합니다.',
@@ -42,7 +43,7 @@ router.post('/signup', async (req, res) => {
     const salt = await Bcrypt.genSalt(Number(process.env.SaltKEY));
     const hashPassword = await Bcrypt.hash(password, salt);
 
-    const user = new User({ userId, password: hashPassword, userImageUrl });
+    const user = new User({ userid, password: hashPassword, profileimage });
     await user.save();
     res.status(201).send({ message: '회원가입 완성' });
   } catch (error) {
@@ -53,35 +54,85 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+
+
+//회원가입 - 기업
+router.post('/api/user/company/signup', async (req, res) => {
+  try {
+    const { userid, password, confirmpassword, profileimage, intro, image, address } =
+      await postUsersSchema.validateAsync(req.body);
+
+    if (password !== confirmPassword) {
+      return res.status(400).send({
+        errorMessage: '패스워드가 패스워드 확인란과 동일하지 않습니다.',
+      });
+    }
+
+    const exitstUsers = await User.find({ userid });
+    if (exitstUsers.length) {
+      return res.status(400).send({
+        errorMessage: '중복된 아이디가 존재합니다.',
+      });
+    }
+
+    const salt = await Bcrypt.genSalt(Number(process.env.SaltKEY));
+    const hashPassword = await Bcrypt.hash(password, salt);
+
+    const cp_user = new CompanyUser({ userid, password: hashPassword, profileimage, intro, image, address });
+    await cp_user.save();
+    res.status(201).send({ message: '회원가입 완성' });
+  } catch (error) {
+    return res.status(400).send(
+      console.error(error)
+      // errorMessage: '요청한 데이터 형식이 올바르지 않습니다.',
+    );
+  }
+});
+
+
+
+
 //로그인
 router.post('/user/login', async (req, res) => {
-  const { userId, password } = req.body;
-  const user = await User.findOne({ userId });
+  const { userid, password } = req.body;
+  const user = await User.findOne({ userid });
+  const cp_user = await CompanyUser.findOne({ userid });
 
-  if (!user) {
+  if (!user||!cp_user) {
     return res.status(400).send({
       errorMessage: '아이디 또는 비밀번호를 확인해주세요.',
     });
   }
 
   const validPassword = await Bcrypt.compare(password, user.password);
-  console.log(validPassword);
+  // console.log(validPassword);
 
   if (!validPassword) {
     return res.send('비밀번호가 틀렸습니다..');
   }
 
-  const token = jwt.sign({ userId: user.userId }, process.env.SECRET_KEY);
+  const token = jwt.sign({ userid: user.userid }, process.env.SECRET_KEY);
   res.send({ token });
 });
+
+
+
+
+
+
+
+
+
+
+
 
 // 정보 조회
 router.get('/checkLogin', authMiddleware, async (req, res) => {
   const { user } = res.locals;
   res.send({
     success: '정보 조회가 성공하였습니다.',
-    userId: user[0].userId,
-    userImageUrl: user[0].userImageUrl,
+    userid: user[0].userid,
+    profileimage: user[0].profileimage,
   });
 });
 
