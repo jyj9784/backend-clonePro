eff288c5db43cf78964223835a326ab4ed53d387
selim-jo <skyblue.777@daum.net>
2022-06-18 09:21:28 +0900

    채용정보 CRUD&회사정보 조회 코드  완성

--- routes/company.js
+++ routes/company.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const CompanyUser = require('../schemas/companyuser');
-const authMiddleware = require('../middlewares/auth-middleware');
+const authMiddlewareCo = require('../middlewares/auth-middleware-co');
 
-// 회사정보 전체조회(로그인 시 가능)
-router.get('/', async (req, res) => {
+// 회사정보 전체조회(로그인 안되도 다 볼 수 있게?)
+router.get('/company', async (req, res) => {
     try {
-        const companyInfo = await Companyuser.find({}, { companyname: 1, profileimage: 1, intro: 1, image: 1, address: 1 })
-        res.json({ comanyInfo });
+        const companyInfo = await CompanyUser.find({}, { companyname: 1, profileimage: 1, intro: 1, image: 1, address: 1 });
+        res.send(companyInfo);
     } catch (err) {
         res.status(400).send("error")
     }
--- schemas/post.js
+++ schemas/post.js
@@ -5,42 +5,43 @@ const PostSchema = new mongoose.Schema({
         postingid: {
             type: Number,
             required: true,
-            unique: true
+            unique: true,
         },
-        // 채용정보 작성자
-        companyname: {
+        // 채용정보 작성자 userid
+        userid: {
             type: String,
-            required: true
+            required: true,
+            unique: true,
         },
         // 채용정보 썸네일
         thumbnail: {
             type: String,
-            required: true
+            required: true,
         },
         // 채용정보 제목
         title: {
             type: String,
-            required: true
+            required: true,
         },
         // 채용정보 주요업무
         maincontent: {
             type: String,
-            required: true
+            required: true,
         },
         // 채용정보 자격요건
         subcontent: {
             type: String,
-            required: true
+            required: true,
         },
         // 채용정보 이미지
         userimage: {
             type: String,
-            required: true
+            required: true,
         },
         // 채용정보 포지션(프론트엔드(1) or 백엔드(-1))
         position: {
             type: String,
-            required: true
+            required: true,
         },   
     },
     // createdAt, updatedAt 자동설정
