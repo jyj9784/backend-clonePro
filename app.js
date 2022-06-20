20c6854c7a3a35baff203577413d443bc73749e1
jyj9784 <jyj2873@naver.com>
2022-06-20 15:47:03 +0900

    chatlab

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
+    credentials : true,
   },
 });
 // SocketIO(server);
@@ -45,32 +45,37 @@ app.get('/', (req, res) => {
   res.send('헬로 월드');
 });
 
-app.get('/chat', (req, res) => {
-  res.sendFile(__dirname + '/chat.html');
-});
-io.on('connection', (socket) => {
-  // socket.send("hihi");
+io.on('connection', socket=>{
+  socket.on('message',({name,message}) => {
+      io.emit('message',({name, message}))
+  })
+})
+// app.get('/chat', (req, res) => {
+//   res.sendFile(__dirname + '/chat.html');
+// });
+// io.on('connection', (socket) => {
+//   // socket.send("hihi");
 
-  socket.on('disconnect', () => {
-    io.emit('send message', {
-      message: `${socket.username} 님께서 채팅창을 떠났습니다. ${createdAt}`,
-      user: '환영합니다',
-    });
-  });
+//   socket.on('disconnect', () => {
+//     io.emit('send message', {
+//       message: `${socket.username} 님께서 채팅창을 떠났습니다. ${createdAt}`,
+//       user: '환영합니다',
+//     });
+//   });
 
-  socket.on('new message', (msg) => {
-    console.log(msg);
-    io.emit('send message', { message: msg, user: socket.username });
-  });
+//   socket.on('new message', (msg) => {
+//     console.log(msg);
+//     io.emit('send message', { message: msg, user: socket.username });
+//   });
 
-  socket.on('new user', (usr) => {
-    socket.username = usr;
-    io.emit('send message', {
-      message: `${socket.username} 님이 채팅에 참여하셨습니다.`,
-      user: 'Welcome!',
-    });
-  });
-});
+//   socket.on('new user', (usr) => {
+//     socket.username = usr;
+//     io.emit('send message', {
+//       message: `${socket.username} 님이 채팅에 참여하셨습니다.`,
+//       user: 'Welcome!',
+//     });
+//   });
+// });
 
 
 server.listen(port, () => {
