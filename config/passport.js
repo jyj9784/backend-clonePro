c0764fa6d826eafaea6e83ef6549beed13e1c808
hayeonkimm <hayeon.data@gmail.com>
2022-06-21 15:16:31 +0900

    업데이트

--- config/passport.js
+++ config/passport.js
@@ -17,7 +17,7 @@ passport.use(new GoogleStrategy(
   {
     clientID      : process.env.GOOGLE_CLIENT_ID,
     clientSecret  : process.env.GOOGLE_SECRET,
-    callbackURL   : 'http://localhost:3000/auth/google/callback',
+    callbackURL   : 'http://hayeon-sum.shop/auth/google/callback',
     passReqToCallback   : true
   }, function(request, accessToken, refreshToken, profile, done){
     console.log('profile: ', profile);
