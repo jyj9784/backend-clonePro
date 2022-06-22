4feca133d0e2b561529dc512902986d2d89abd35
hayeonkimm <hayeon.data@gmail.com>
2022-06-22 20:24:45 +0900

    파일 정리 및 업데이트

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const usersRouter = require('./routes/users');
 const companyRouter = require('./routes/company');
 const comypageRouter = require('./routes/mypage_co');
 const mypageRouter = require('./routes/mypage');
+const mainRouter = require('./routes/main');
+const G_authRouter = require('./routes/google_auth');
 const passport = require('passport');
 const { Server } = require('socket.io');
 const swaggerUi = require('swagger-ui-express');
@@ -21,8 +23,6 @@ const swaggerFile = require('./swagger_output');
 const http = require('http');
 const server = http.createServer(app);
 const Msg = require('./schemas/messages');
-const mainRouter = require('./routes/main');
-const G_authRouter = require('./routes/google_auth');
 const cookieParser = require('cookie-parser');
 
 connect();
@@ -48,58 +48,53 @@ app.use(
 // Passport setting
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Routes
 app.use('/', require('./routes/main'));
 app.use('/auth', require('./routes/google_auth'));
-app.use('/api', [usersRouter, postsRouter, companyRouter, comypageRouter, mypageRouter]);
+app.use('/api', [
+  usersRouter,
+  postsRouter,
+  companyRouter,
+  comypageRouter,
+  mypageRouter,
+]);
 app.use('/auth', [mainRouter, G_authRouter]);
 app.get('/', (req, res) => {
   res.send('헬로 월드');
 });
-
-
-// app.get('/chat', (req, res) => {
-//   res.sendFile(__dirname + '/chat.html');
-// });
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-
-
-
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: '*',
+    methods: ['GET', 'POST'],
   },
 });
 const chatspace = io.of('/chat');
-chatspace.on("connection", (socket) => {
+chatspace.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
+  socket.on('join_room', (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
-  socket.on("send_message", (data) => {
+  socket.on('send_message', (data) => {
     const message = new Msg(data);
     message.save().then(() => {
-    socket.to(data.room).emit("receive_message", data);
-    
-    console.log(data)
-  });
+      socket.to(data.room).emit('receive_message', data);
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+      console.log(data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User Disconnected', socket.id);
+    });
   });
 });
-});
-
 
 server.listen(port, () => {
   console.log(port, '포트가 켜졌습니다.');
--- schemas/googleUser.js
+++ schemas/googleUser.js
@@ -1,9 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-const { boolean } = require('joi');
-
 const GuserSchema = new mongoose.Schema({
   username: {
     type: String,
